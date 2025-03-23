e9512468e8f459222893281934442388ff1a7f01
Navid Yaghoobi <navidys@fedoraproject.org>
2025-03-23 18:49:22 +1100

    distribution-spec + fixes on runtime-spec
    Signed-off-by: Navid Yaghoobi <navidys@fedoraproject.org>

---
+++ examples/artifact_manifest.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const ocispec = @import("oci-spec");
+const image = ocispec.image;
+
+pub fn main() !void {
+    const file_path = "./tests/fixtures/artifact_manifest.json";
+    const artifact_manifest = try image.ArtifactManifest.initFromFile(file_path);
+
+    const config_content = try artifact_manifest.toStringPretty();
+
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("{s}\n", .{config_content});
+
+    try bw.flush();
+}
--- src/lib.zig
+++ src/lib.zig
@@ -1,2 +1,3 @@
 pub const image = @import("image/lib.zig");
 pub const runtime = @import("runtime/lib.zig");
+pub const distribution = @import("distribution/lib.zig");
